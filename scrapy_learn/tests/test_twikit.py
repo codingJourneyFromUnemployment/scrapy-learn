a8af2fe66b42e90a55e838e66f221e86797c4538
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2024-06-18 15:14:08 +0800

    test twikit

---
+++ scrapy_learn/tests/test_twikit.py
@@ -0,0 +1,50 @@
+from dotenv import load_dotenv
+import os
+from twikit import Client 
+
+def load_credentials():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+
+    project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+
+    dotenv_path = os.path.join(project_root, '.env')
+
+    load_dotenv(dotenv_path)
+
+    user = os.getenv('USERNAME')
+
+    email = os.getenv('EMAIL')
+
+    password = os.getenv('PASSWORD')
+    
+    secur_code = input('Enter your security code: \n')
+    
+    real_password = password + secur_code
+    
+    google_auth = input('Enter your google auth code: \n')
+    
+    return {
+      "user": user,
+      "email": email,
+      "password": real_password,
+      "google_auth": google_auth
+    }
+    
+def client_login():
+    credentials = load_credentials()
+    client = Client('en-US')
+    
+    client.login(
+      auth_info_1=credentials['user'],
+      auth_info_2=credentials['email'],
+      password=credentials['password'],
+      totp_secret=credentials['google_auth'],
+    )
+    
+    return client
+    
+test_client = client_login()
+test_user = test_client.get_user_by_id("ericsmith1302")
+print(test_user)
+
+      
