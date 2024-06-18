9e3a04351893b6217097e7daa833abd14fd7ce9c
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-06-18 08:19:03 +0000

    test codespaces

--- scrapy_learn/tests/test_twikit.py
+++ scrapy_learn/tests/test_twikit.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
 import os
-from twikit import Client 
+from twikit import Client
+from colorama import Fore, Style, init, Back 
 
 def load_credentials():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
 
-    project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
+    # project_root = os.path.abspath(os.path.join(current_dir, os.pardir, os.pardir))
 
-    dotenv_path = os.path.join(project_root, '.env')
+    # dotenv_path = os.path.join(project_root, '.env')
 
-    load_dotenv(dotenv_path)
+    # load_dotenv(dotenv_path)
 
     user = os.getenv('USERNAME')
 
@@ -34,22 +35,30 @@ def client_login():
     credentials = load_credentials()
     client = Client('en-US')
     
-    client.login(
-      auth_info_1=credentials['user'],
-      auth_info_2=credentials['email'],
-      password=credentials['password'],
-      totp_secret=credentials['google_auth'],
-    )
+    print(credentials['user'], credentials['email'], credentials['password'], credentials['google_auth'])
+    
+    try:
+        client.login(
+          auth_info_1=credentials['user'],
+          auth_info_2=credentials['email'],
+          password=credentials['password'],
+          totp_secret=credentials['google_auth'],
+        )
+    except Exception as e:
+        print(e)
     
     return client
     
 try:
     print("Logging in...")
     test_client = client_login()
-    print("Login successful")
-    test_user = test_client.get_user_by_id("ericsmith1302")
-    print(test_user)
+    print(Fore.GREEN + "Login successful!" + Style.RESET_ALL)
+    cookie = test_client.get_cookie()
+    print(Fore.GREEN + "Cookie found!" + Style.RESET_ALL)
+    print(cookie)
 except Exception as e:
+    print(Fore.RED + "Error:" + Style.RESET_ALL)
     print(e)
+
 
       
